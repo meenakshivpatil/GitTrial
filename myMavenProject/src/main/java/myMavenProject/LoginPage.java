a196ddc7be526b6c9e963eb04d9aba5740b47792
Meenakshi Patil <meenakshivpatil@gmail.com>
2018-09-25 19:46:16 -0500

    added Testng

--- myMavenProject/src/main/java/myMavenProject/LoginPage.java
+++ myMavenProject/src/main/java/myMavenProject/LoginPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class LoginPage {
 	//public WebDriver driver;
