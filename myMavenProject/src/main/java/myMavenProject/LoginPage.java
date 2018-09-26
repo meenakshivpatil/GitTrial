72f3d7c227913328222e74a27dbe84936732133b
Meenakshi Patil <meenakshivpatil@gmail.com>
2018-09-25 20:18:35 -0500

    changed LoginPage.java

--- myMavenProject/src/main/java/myMavenProject/LoginPage.java
+++ myMavenProject/src/main/java/myMavenProject/LoginPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import myMavenProject.PageFactoryExample;
 
 public class LoginPage {
 	//public WebDriver driver;
